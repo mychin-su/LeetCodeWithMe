f6ef8a2e875795977ffab2ae63c126f6a13c095e
Vương Đức Thoại <125477357+vuongducthoai@users.noreply.github.com>
2024-03-07 08:33:23 +0700

    7/3/2024

---
+++ src/Middle_of_the_Linked_List_876.java
@@ -0,0 +1,43 @@
+
+public class Middle_of_the_Linked_List_876 {
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		};
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		ListNode n5 = new ListNode(5);
+
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		System.out.println(middleNode(n1));
+	}
+}
